87f7ee7dc28812c029f03d8f52cf8977a35ff87e
diamimi <bigmheqi@sina.com>
2017-06-04 00:05:41 +0800

    正常提交

--- src/main/java/cn/sioo/thread/BaseThread.java
+++ src/main/java/cn/sioo/thread/BaseThread.java
@@ -1,6 +1,5 @@
 package cn.sioo.thread;
 
-import cn.sioo.pojo.SmsUserConsume;
 import cn.sioo.service.BaseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,24 +13,24 @@ public  class BaseThread<T> implements Runnable{
 
     public static Logger LOGGER = LoggerFactory.getLogger(BaseThread.class);
 
-    public BaseService t;
+    public BaseService baseService;
 
-    public BaseThread(BaseService t) {
+    public BaseThread(BaseService baseService) {
 
-        this.t=t;
+        this.baseService=baseService;
     }
 
     @Override
     public void run() {
         long begin=System.currentTimeMillis();
-        int count31=t.findCount31();
-        int count21=t.findCount21();
+        int count31=baseService.findCount31();
+        int count21=baseService.findCount21();
         int size=2000;
         int part=count21/size;
         for(int i=0;i<=part;i++){
-            List<T> list = t.findList(count31, size);
+            List<T> list = baseService.findList(count31, size);
             if(list.size()>0){
-                t.insertBatch(list);
+                baseService.insertBatch(list);
             }else {
                 break;
             }
