1349ac4a1f21f4d90e1dffb417a15c8e66b55a3e
morrigan <bigmheqi@sina.com>
2017-06-07 11:43:19 +0800

    正常提交

--- src/main/java/cn/sioo/pojo/BaseEntity.java
+++ src/main/java/cn/sioo/pojo/BaseEntity.java
@@ -5,68 +5,64 @@ import java.io.Serializable;
 
 /**
  * 基类、包含分页和ID
- * 
+ *
  * @author CQL  331737188@qq.com
  * @date : 2015年9月23日 下午3:58:51
- *
  */
-public class BaseEntity implements Serializable{
+public class BaseEntity implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
+    @Transient
+    private Integer pageIndex = Integer.valueOf(0);    //起始页索引
 
+    @Transient
+    protected Integer pageSize = Integer.valueOf(15);    //每页大小
 
+    @Transient
+    protected Integer currentPageIndex;
 
-	@Transient
-	private Integer pageIndex = Integer.valueOf(0);	//起始页索引
-
-	@Transient
-	protected Integer pageSize = Integer.valueOf(15);	//每页大小
-
-	@Transient
-	protected Integer currentPageIndex;
-
-	@Transient
-	protected Integer currentPage;	//当前页
+    @Transient
+    protected Integer currentPage;    //当前页
 
 
-	public String getOrderBy() {
-		return "id";
-	}
+    public String getOrderBy() {
+        return "id";
+    }
 
-	public Integer getPageIndex() {
-		return pageIndex;
-	}
+    public Integer getPageIndex() {
+        return pageIndex;
+    }
 
-	public void setPageIndex(Integer pageIndex) {
-		this.pageIndex = pageIndex;
-	}
+    public void setPageIndex(Integer pageIndex) {
+        this.pageIndex = pageIndex;
+    }
 
-	public Integer getPageSize() {
-		return pageSize;
-	}
+    public Integer getPageSize() {
+        return pageSize;
+    }
 
-	public void setPageSize(Integer pageSize) {
-		this.pageSize = pageSize;
-	}
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
 
-	public Integer getCurrentPage() {
-		return currentPage;
-	}
+    public Integer getCurrentPage() {
+        return currentPage;
+    }
 
-	public void setCurrentPage(Integer currentPage) {
-		this.currentPage = currentPage;
-	}
+    public void setCurrentPage(Integer currentPage) {
+        this.currentPage = currentPage;
+    }
 
-	public Integer getCurrentPageIndex() {
-		return currentPageIndex;
-	}
+    public Integer getCurrentPageIndex() {
+        return currentPageIndex;
+    }
 
-	public void setCurrentPageIndex(Integer currentPageIndex) {
-		this.currentPageIndex = currentPageIndex;
-	}
-	
+    public void setCurrentPageIndex(Integer currentPageIndex) {
+        this.currentPageIndex = currentPageIndex;
+    }
+
 }
